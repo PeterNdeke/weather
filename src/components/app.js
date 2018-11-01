52ef9b8771c09e1983b2a39f28befb25913a04c3
PeterNdeke <35370838+PeterNdeke@users.noreply.github.com>
2018-11-01 10:09:57 +0000

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Component } from "react";
+
+import SearchBar from "../containers/search_bar";
+import WeatherList from "../containers/weather_list";
+import Navabar from "../containers/navabar";
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Navabar />
+        <SearchBar />
+        <WeatherList />
+      </div>
     );
   }
 }
